8d7fe648c5198088bbd42b8cbfdd33819730765a
kiteB <mmy789@sch.ac.kr>
2021-11-12 12:56:47 +0900

    [BE][연주] Refactor: import문 수정

--- account/models.py
+++ account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
-import re
+
 
 class User(AbstractUser):
     nickname = models.CharField(max_length=50)
