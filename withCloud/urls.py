4b96e21a83518fa4520b1d06fcd044f2ed15022e
Yebin <lyb2325@gmail.com>
2021-11-12 17:12:21 +0900

    [BE][예빈] Refactor: 프로젝트 urls에 space app의 url 추가

--- withCloud/urls.py
+++ withCloud/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from account import views as a
+from space import views as s 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', a.home, name="home"),
+    path('account/', include('account.urls')),
+    path('space/', include('space.urls')),
 ]
