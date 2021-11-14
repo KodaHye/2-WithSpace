36070c49c6c72cc9c7886841938390f554416f2d
Yebin <lyb2325@gmail.com>
2021-11-15 04:57:04 +0900

    [예빈][BE] Refactor: regex의 정규식 코드 수정 (시작: 01)

--- space/models.py
+++ space/models.py
@@ -28,8 +28,8 @@ class Space(models.Model):
         choices=SPACE_TYPE_CHOICES,
         default='STUDY'
     )
-    contactNumberRegex = RegexValidator(regex = r"^\+?1?\d{8,15}$")
-    contact_number = models.CharField(validators = [contactNumberRegex], max_length = 16, unique = True)
+    contactNumberRegex = RegexValidator(regex = r'^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$')
+    contact_number = models.CharField(validators = [contactNumberRegex], max_length = 11, unique = True)
     price = models.IntegerField() # 시간당 가격
     space_brief_detail = models.CharField(max_length=200)
     space_detail = models.TextField()
