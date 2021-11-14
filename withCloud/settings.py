cd12014c36d40f241b2b4c49b45dc7201ca213b6
Yebin <lyb2325@gmail.com>
2021-11-15 05:13:27 +0900

    [FE][예빈] Refactor: static files 경로 설정, base.html 상속 설정

--- withCloud/settings.py
+++ withCloud/settings.py
@@ -139,6 +139,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'space', 'static'),
+    os.path.join(BASE_DIR, 'account', 'static'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
